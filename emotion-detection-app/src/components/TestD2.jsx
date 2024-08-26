09c56f6cbf9e579a3a7ade062a3a5189c2bb298a
sddrouet <sddrouet@espe.edu.ec>
2024-08-25 20:05:27 -0500

    diseño UI TESTD2R mejorado

--- emotion-detection-app/src/components/TestD2.jsx
+++ emotion-detection-app/src/components/TestD2.jsx
@@ -1,27 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { initialTestData } from '../utils/test';
 
 const TestD2 = () => {
-    const initialTestData = [
-        ["d'", "d", "p", 'p"', "p'", "d'", 'd"', '"d', "d'", "p'", "p", "p'", 'd"', 'p"', "p'", "d", "d'", "p", "p", "d'", '"d', "d'", "d", "p'", "p'", 'p"', "d'", "d'", "d", "d'", '"d', "d'", "p", "d'", 'd"', "d'", "p'", "p", "d", "p'", "d'"],
-        ["p'", 'p"', 'd"', "p'", "d", "d'", "p", "d'", "p", "d'", 'd"', "p", "p", "p", "p", 'p"', "d", "d'", '"d', "p", "p'", "p'", "d'", '"d', "p'", "p", "p", "d'", "p'", 'p"', "d", "d", "d", "d'", 'p"', "d", "d", "p'", "d", "p", "d'"],
-        ['d"', "p", "p", 'p"', "p", "p", "p'", 'd"', "d", "p", "d", "d'", "d'", "d'", "p'", "p", 'p"', "d", "d'", "d", "p'", '"d', "d'", "p'", "p", 'd"', "d'", "d'", '"d', "p'", "p", "p", 'd"', "p", 'p"', "p'", "d", "d", "d", "d'", 'd"'],
-        ["d'", 'd"', "p'", 'p"', "p", "d'", "d", "p'", "p", "d", 'd"', "p", "p", 'd"', 'd"', 'p"', "d", "p'", '"d', "p", "d", "p", "p", 'd"', "d", "d", 'p"', "d'", 'p"', "d'", "p", "d", "p'", "d'", 'p"', "d", "p'", "d", "d", "p", "d'"],
-        ["p", "p'", "p'", "p'", "p", "p'", "d", "d", "p'", "p'", "d", "d'", "p", "d'", "d'", "p'", "p'", 'd"', "d", "p'", '"d', "p", "p'", 'd"', "p", "p'", "p'", "p", "p", 'd"', "d'", "p'", "d'", "p", 'd"', "p", "p'", "d", "p", "d", "d'"],
-        ["p'", "p", "p", "d", "p'", "p", "d'", "p", "p'", "p'", "p'", "p", "d'", '"d', "d'", "p", "p", "p'", "d", "p", "p'", "d'", "p'", "p", "p", "p", "d", 'd"', "d", "p", "d", "d", "p", 'd"', 'd"', "p'", "p", "d'", "d", 'd"', "d'"],
-        ["d'", "p", "p", 'd"', "d'", "p'", 'p"', "d'", 'd"', "p'", "p", "d'", 'p"', "d'", "p", 'p"', "p", "p'", 'd"', "p'", "p", "d", "p'", 'd"', "p", "p", "d'", "p'", "p", 'd"', "d'", "d'", "p'", "p", "p'", 'p"', "p", "d", "d", "d", "d'"],
-        ["d", "p'", "d", "p", 'd"', 'p"', "d'", "p'", "d", "d'", "p", "p'", "p'", "d'", "p", 'p"', "d", "d'", "p'", "p", "p", "d'", "d'", "p", "p", "p'", "d", "p", "p'", 'd"', 'p"', "d", "p", "d'", "d", "p", "p", "d'", "d'", "p", "d'"],
-        ["d'", "p'", "d'", 'p"', "p'", "p", "p", "d'", 'd"', 'd"', "d", "p'", "p'", "d", "d", "d", "d'", "d", "p'", "p", "p", "p'", "p", "d'", "p", "p", "p", 'p"', "p'", "d'", "d", "p", "p", "d", "p", "d", "p'", "p", "p", "p'", "p"],
-        ['p"', "p", "d", "p", "p", "d'", "d'", 'd"', "p", 'p"', "p", 'p"', 'p"', "d'", 'd"', "p", "d'", 'd"', "p", "p", "d", 'd"', "d'", "p", "p'", "p'", "d'", "p", "d", "d", "p'", "p", "p", "p", "p", "p", "p'", "p", "d", "p", "d'"],
-        ["p", "p", "d'", "d", "p'", "d'", "p'", "p", 'p"', "p", "p'", "d'", "p", 'd"', "p", "p", 'p"', "p", "p", "d'", "d", "p", "p'", "d'", "d", "d", "d", "p", 'd"', "p", 'p"', "d'", 'd"', "d'", 'd"', "p", "p", "p", 'd"', "d", "d'"],
-        ['p"', "d'", "p", "d", "p", "p", "p", 'p"', "d", "p'", "d", "d", "p'", "d'", "d", "p'", 'd"', "p", 'p"', "p'", "p", "d", "p'", "p", "p'", "p'", "p'", "p", "d", "d", "p", "p'", "p'", 'p"', "d", "d'", "d'", "p", "d'", 'd"', 'd"'],
-        ["d", "p", "p", "d'", "d'", "p", "d", "p", "d'", "p", "d", "p", "d", 'd"', 'd"', "p", "d'", "p", "d", "d'", "p", "p'", "p", "d'", "d", "p'", "p", 'p"', "p", "d", "p", "p", "p", "d", "p", "p'", "p", "p", "p", "d", "p"],
-        ["d'", 'd"', "p'", "p'", "d'", "p'", 'p"', "d", "d'", "p", "d'", "p", "p'", 'd"', "d", "d'", "p", "p", "p", "p", "d'", 'p"', "d'", "p'", "p'", 'd"', "d", "p", "p", 'p"', "d", "p", "p", 'p"', "d", 'd"', "p", "p", "p", "d", "d'"],
-    ];
-
     const [checked, setChecked] = useState(initialTestData.map(row => Array(row.length).fill(false)));
+    const [corrects, setCorrects] = useState(initialTestData.map(row => Array(row.length).fill(0)));
     const [isStarted, setIsStarted] = useState(false);
     const [time, setTime] = useState(0);
     const [isFinished, setIsFinished] = useState(false);
+    const [disabledRow, setDisabledRow] = useState(-1); // Nueva variable de estado para deshabilitar filas
     const [results, setResults] = useState({
         total: 0,
         correct: 0,
@@ -29,22 +15,31 @@ const TestD2 = () => {
         faltantes: 0
     });
 
-    // Manejo del cronómetro
+    // Manejo del cronómetro y deshabilitar filas cada 20 segundos
     useEffect(() => {
         let intervalId;
 
         if (isStarted && !isFinished) {
             intervalId = setInterval(() => {
                 setTime(prevTime => prevTime + 1);
+                if (time > 0 && time % 20 === 0) {
+                    setDisabledRow(prevRow => prevRow + 1);
+                }
             }, 1000);
         } else {
             clearInterval(intervalId);
         }
 
+        if (disabledRow >= initialTestData.length) {
+            handleFinish();
+        }
+
         return () => clearInterval(intervalId);
-    }, [isStarted, isFinished]);
+    }, [isStarted, isFinished, time, disabledRow]);
 
     const handleCheck = (rowIndex, colIndex) => {
+        if (rowIndex <= disabledRow) return; // Deshabilitar interacción si la fila está deshabilitada
+
         const newChecked = [...checked];
         newChecked[rowIndex][colIndex] = !newChecked[rowIndex][colIndex];
         setChecked(newChecked);
@@ -62,18 +57,21 @@ const TestD2 = () => {
             row.forEach((item, colIndex) => {
                 if (item === '"d' || item === 'd"') {
                     total++;
-                    if (checked[rowIndex][colIndex] && (item === '"d' || item === 'd"')) {
+                    if (checked[rowIndex][colIndex]) {
                         correct++;
-                    } 
+                        corrects[rowIndex][colIndex] = 1;
+                    } else {
+                        corrects[rowIndex][colIndex] = 2;
+                    }
                 }
-
                 if (checked[rowIndex][colIndex] && (item !== '"d' && item !== 'd"')) {
                     incorrect++;
+                    corrects[rowIndex][colIndex] = 3;
                 }
             });
-            faltantes = total - correct;
         });
 
+        faltantes = total - correct;
         setResults({ total, correct, incorrect, faltantes });
     };
 
@@ -83,6 +81,17 @@ const TestD2 = () => {
         setChecked(initialTestData.map(row => Array(row.length).fill(false)));
         setResults({ total: 0, correct: 0, incorrect: 0, faltantes: 0 });
         setTime(0);
+        setDisabledRow(-1); // Reinicia la deshabilitación de filas
+    };
+
+    const handleRestart = () => {
+        setIsStarted(false);
+        setIsFinished(false);
+        setChecked(initialTestData.map(row => Array(row.length).fill(false)));
+        setCorrects(initialTestData.map(row => Array(row.length).fill(0)));
+        setResults({ total: 0, correct: 0, incorrect: 0, faltantes: 0 });
+        setTime(0);
+        setDisabledRow(-1);
     };
 
     const formatTime = (time) => {
@@ -110,26 +119,17 @@ const TestD2 = () => {
                     <div className="flex justify-between items-center mb-4">
                         <span className="text-lg font-bold">{`Tiempo: ${formatTime(time)}`}</span>
                     </div>
-                    <table className="table-auto w-full">
+                    <table className="table-fixed w-full border-collapse">
                         <tbody>
                             {initialTestData.map((row, rowIndex) => (
-                                <tr key={rowIndex}>
+                                <tr key={rowIndex} className='border-b-4 border-gray-500'>
                                     {row.map((item, colIndex) => (
-                                        <td
-                                            key={colIndex}
-                                            className="p-2 border"
-                                        >
-                                            <div className="flex items-center cursor-pointer hover:bg-gray-200"
-                                                onClick={() => handleCheck(rowIndex, colIndex)}
+                                        <td key={colIndex}>
+                                            <div
+                                                className={`${
+                                                    corrects[rowIndex][colIndex] === 1 ? "bg-green-400" : corrects[rowIndex][colIndex] === 2 ? "bg-neutral-400" : corrects[rowIndex][colIndex] === 3 ? "bg-red-400" : "bg-transparent"
+                                                } text-xl p-1 font-semibold flex items-center rounded justify-center w-full my-2`}
                                             >
-                                                <div className='u'>
-                                                    <input
-                                                        type="checkbox"
-                                                        checked={checked[rowIndex][colIndex]}
-                                                        onChange={() => handleCheck(rowIndex, colIndex)}
-                                                        className="mr-2 cursor-pointer"
-                                                    />
-                                                </div>
                                                 <span>{item}</span>
                                             </div>
                                         </td>
@@ -145,33 +145,31 @@ const TestD2 = () => {
                         <p>Marcados incorrectos: <span className="font-bold">{results.incorrect}</span></p>
                         <p>Faltantes: <span className="font-bold">{results.faltantes}</span></p>
                     </div>
+                    <button onClick={handleRestart} className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mt-4">
+                        Reiniciar Test
+                    </button>
                 </>
             ) : (
                 <>
                     <div className="flex justify-between items-center mb-4">
                         <span className="text-lg font-bold">{`Tiempo: ${formatTime(time)}`}</span>
-                        <button onClick={handleFinish} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+                        <button onClick={handleFinish} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
                             Finalizar
                         </button>
                     </div>
-                    <table className="table-auto w-full">
+                    <table className="table-fixed w-full select-none">
                         <tbody>
                             {initialTestData.map((row, rowIndex) => (
-                                <tr key={rowIndex}>
+                                <tr key={rowIndex} className={`border-b-4 border-gray-500 ${disabledRow >= rowIndex ? 'opacity-50' : ''}`}>
+                                    <td><p className='font-semibold'>{rowIndex + 1}.</p></td>
                                     {row.map((item, colIndex) => (
-                                        <td
-                                            key={colIndex}
-                                            className="p-2 border cursor-pointer hover:bg-gray-200"
-                                            onClick={() => !isFinished && handleCheck(rowIndex, colIndex)}
-                                        >
-                                            <div className="flex items-center">
-                                                <input
-                                                    type="checkbox"
-                                                    checked={checked[rowIndex][colIndex]}
-                                                    onChange={() => handleCheck(rowIndex, colIndex)}
-                                                    className="mr-2"
-                                                    disabled={isFinished}
-                                                />
+                                        <td key={colIndex}>
+                                            <div
+                                                className={`${
+                                                    checked[rowIndex][colIndex] ? "bg-green-400" : rowIndex % 2 === 0 ? 'bg-gray-300' : 'bg-gray-200'
+                                                } text-2xl p-2 font-semibold flex items-center justify-center w-full cursor-pointer hover:bg-opacity-50 border-none border-collapse my-4 rounded`}
+                                                onClick={() => !isFinished && handleCheck(rowIndex, colIndex)}
+                                            >
                                                 <span>{item}</span>
                                             </div>
                                         </td>
